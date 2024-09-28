b927c7fb7c318280210e50325d5ddb2a3540ecb6
marwa <meroblue25@gmail.com>
2024-09-29 00:51:12 +0300

    add basic admin navigation

---
+++ app/(tabs)/(profile)/(admin)/adminProfile.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+
+import { View, Text} from "react-native";
+import React from "react";
+import { COLORS } from "@/constants/theme";
+
+
+
+const AdminProfile = () => {
+  return (
+    <View>
+      <Text style={{ color: COLORS.primary }}> Admin Profile</Text>
+   
+    </View>
+  );
+};
+
+export default AdminProfile;
