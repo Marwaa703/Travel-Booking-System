e7d12ad058c2a305025c09790b9f623ff7b2191b
yomna <yomna.farid.mohamed@gmail.com>
2024-09-28 19:55:59 +0300

    modification on the setting card

--- app/(tabs)/(home)/home.tsx
+++ app/(tabs)/(home)/home.tsx
@@ -3,7 +3,7 @@ import { View, Text, Button } from "react-native";
 import React from "react";
 import { router } from "expo-router";
 // import { COLORS } from "@/constants/theme";
-// import SettingCard from "@/components/SettingContainer";
+import SettingCard from "@/components/SettingContainer";
 import NotificationCard from "@/components/NotificationCard";
 // import TripProfileCard from "@/components/TripProfileCard";
 // import InputField from "@/components/InputField";
@@ -38,7 +38,7 @@ const Home = () => {
       {/* <Card image={require("@/assets/tree.jpg")} title="Casa Las Tirtugas" subtitle="Av Damero, Mexico" rating={3.5} price="$894"></Card>
       <Card image={require("@/assets/tree.jpg")} title="Casa Las Tirtugas" subtitle="Av Damero, Mexico" rating={3.5} buttonText="Subscripe"></Card> */}
       {/* <TripProfileCard title="Santorini Islnd" image={require("@/assets/tree.jpg")} date="16 July-28 July" rating={4.5} price="820" peopleJoined={42} avatars={[require("@/assets/tree.jpg"),require("@/assets/tree.jpg"),require("@/assets/tree.jpg")]}></TripProfileCard> */}
-      {/* <SettingCard title="Current Trips" onPress={()=>{}} rigthIconName="chevron-right" leftIconName="bookmark-border"></SettingCard> */}
+      <SettingCard title="Current Trips" onPress={()=>{}} leftIconName="bookmark-border"></SettingCard>
       <NotificationCard avatar={require("@/assets/tree.jpg")} title="Super Offer" description="Get 60% off in our first booking" onPress={()=>{}}></NotificationCard>
       <Button
         onPress={() => {
